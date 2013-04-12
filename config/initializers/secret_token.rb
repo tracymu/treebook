c34c36f5916cf8b2680e600b201076096e698240
Tracy Mu Sung <tracy.musung@moomumedia.com>
2013-04-12 20:04:58 +1000

    I bet I can't find this again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracecode::Application.config.secret_token = '9be912334023f2192a9873ab017785ae7684346e712f8701a236284718dd7d57848d5d9e165d7f5fa1f853caeb576c0660f96081f6b55b1cf8fc62c64cce43c1'
